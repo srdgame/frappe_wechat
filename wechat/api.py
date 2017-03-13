aa5eab59b837ea0dba13a49b4fdfb4d6caedb929
Dirk Chang <dirk@kooiot.com>
2017-03-13 14:21:33 +0800

    wechat api

---
+++ wechat/api.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015, Dirk Chang and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+import json
+import requests
+from frappe import throw, msgprint, _
+from frappe.model.document import Document
+from frappe.utils import cint
+from wechat.doctype.wechat_binding.wechat_binding import wechat_bind, wechat_unbind
+
+
+def valid_auth_code(app=None, auth_code=None):
+	app = app or frappe.get_request_header("App")
+	if not app:
+		throw(_("App is required in HTTP Header!"))
+	auth_code = auth_code or frappe.get_request_header("AuthorizationCode")
+	if not auth_code:
+		throw(_("AuthorizationCode is required in HTTP Header!"))
+	frappe.logger(__name__).debug(_("App as {0} AuthorizationCode as {1}").format(app, auth_code))
+
+	if auth_code != frappe.get_value("Wechat App", app, "authorization_code"):
+		throw(_("Authorization Code is incorrect!"))
+
+	frappe.session.user = frappe.get_value("Wechat App", app, "on_behalf")
+	return app
+
+
+@frappe.whitelist(allow_guest=True)
+def get(user=None):
+	app = valid_auth_code()
+	if not (user):
+		user = frappe.form_dict.get('user')
+	return frappe.get_value("Wechat Binding", {"user":user, "app": app}, "openid")
+
+
+def get_post_json_data():
+	if frappe.request.method != "POST" and frappe.request.method != "PUT":
+		throw(_("Request Method Must be POST!"))
+	ctype = frappe.get_request_header("Content-Type")
+	if "json" not in ctype.lower():
+		throw(_("Incorrect HTTP Content-Type found {0}").format(ctype))
+	if not frappe.form_dict.data:
+		throw(_("JSON Data not found!"))
+	return json.loads(frappe.form_dict.data)
+
+
+@frappe.whitelist(allow_guest=True)
+def bind(frm_data):
+	app = valid_auth_code()
+	frm_data = frm_data or get_post_json_data()
+	user = frm_data.get("user")
+	passwd = frm_data.get("passwd")
+	openid = frm_data.get("openid")
+	expires = frm_data.get("expires")
+	if not (user and passwd and openid):
+		throw(_("user, passwd, openid is required!"))
+
+	frappe.logger(__name__).debug(_("Wechat App binding user {0} password {1} openid {2} expires{3}")
+									.format(user, passwd, openid, expires))
+
+	frappe.local.login_manager.authenticate(user, passwd)
+	if frappe.local.login_manager.user != user:
+		throw(_("Username password is not matched!"))
+
+	return wechat_bind(app, user, openid, expires)
+
+
+
+@frappe.whitelist(allow_guest=True)
+def unbind(user):
+	app = valid_auth_code()
+	user = user or get_post_json_data().get("user")
+	if not (user):
+		throw(_("user is required!"))
+
+	frappe.logger(__name__).debug(_("Wechat App binding user {0}").format(user))
+
+	return wechat_unbind(app, user)
+
+
+@frappe.whitelist(allow_guest=True)
+def get_time():
+	valid_auth_code()
+	return frappe.utils.now()
+
+
+@frappe.whitelist(allow_guest=True)
+def ping():
+	form_data = frappe.form_dict
+	if frappe.request and frappe.request.method == "POST":
+		if form_data.data:
+			form_data = json.loads(form_data.data)
+		return form_data.get("text") or "No Text"
+	return 'pong'
+
+
--- wechat/wechat/doctype/wechat_binding/wechat_binding.py
+++ wechat/wechat/doctype/wechat_binding/wechat_binding.py
@@ -4,7 +4,37 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import throw, _
 from frappe.model.document import Document
 
+
 class WechatBinding(Document):
-	pass
+	def autoname(self):
+		self.name = '[' + self.app + '].' + self.user
+
+
+@frappe.whitelist()
+def wechat_bind(app, user, openid, expires=None):
+	doc = frappe.get_doc({
+		"doctype": "Wechat Binding",
+		"user": user,
+		"app": app,
+		"openid": openid,
+		"expires": expires
+	})
+	doc.insert()
+	frappe.db.commit()
+
+	return _("Binding is done")
+
+
+@frappe.whitelist()
+def wechat_unbind(app, user):
+	name = frappe.get_value("Wechat Binding", {"app": app, "user": user})
+	if not name:
+		throw(_("There is no binding for App{0} User{1}").format(app, user))
+
+	frappe.delete_doc("Wechat Binding", name)
+	frappe.db.commit()
+
+	return _("Binding has ben deleted")
