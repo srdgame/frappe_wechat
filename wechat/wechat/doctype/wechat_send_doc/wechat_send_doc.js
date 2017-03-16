7c3a21a44d41e4247e43f59a642392222deaa680
Dirk Chang <dirk.chang@symid.com>
2017-03-16 01:45:25 -0700

    update doctype

---
+++ wechat/wechat/doctype/wechat_send_doc/wechat_send_doc.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, Dirk Chang and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Wechat Send Doc', {
+	refresh: function(frm) {
+
+	}
+});
