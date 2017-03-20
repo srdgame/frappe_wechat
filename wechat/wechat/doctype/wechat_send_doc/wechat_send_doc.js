0fb586fcfb234861c42e5b594c1e02d41b71f5c0
Dirk Chang <dirk@kooiot.com>
2017-03-20 16:08:25 +0800

    update document

--- wechat/wechat/doctype/wechat_send_doc/wechat_send_doc.js
+++ wechat/wechat/doctype/wechat_send_doc/wechat_send_doc.js
@@ -3,15 +3,15 @@
 
 frappe.ui.form.on('Wechat Send Doc', {
 	setup: function(frm) {
-		/* frm.fields_dict["doc_type"].get_query = function(){
+		/* frm.fields_dict["document_type"].get_query = function(){
 			return {
 				filters: {
 					"name": ["in","IOT Device Error,Repair Issue,ToDo,User"]
 				}
 			}
 		}; */
-		frm.fields_dict["doc_id"].get_query = function(){
-			if (frm.fields_dict["doc_type"].value === "User") {
+		frm.fields_dict["document_id"].get_query = function(){
+			if (frm.fields_dict["document_type"].value === "User") {
 				return {
 					filters: {"ignore_user_type": 1}
 				};
