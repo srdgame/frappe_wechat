47b5e51980d08b7ca0deb8d85647074bad6cb62d
Dirk Chang <dirk@kooiot.com>
2017-03-20 13:08:41 +0800

    using select instead of link

--- wechat/wechat/doctype/wechat_send_doc/wechat_send_doc.js
+++ wechat/wechat/doctype/wechat_send_doc/wechat_send_doc.js
@@ -3,13 +3,13 @@
 
 frappe.ui.form.on('Wechat Send Doc', {
 	setup: function(frm) {
-		frm.fields_dict["doc_type"].get_query = function(){
+		/* frm.fields_dict["doc_type"].get_query = function(){
 			return {
 				filters: {
 					"name": ["in","IOT Device Error,Repair Issue,ToDo,User"]
 				}
 			}
-		};
+		}; */
 		frm.fields_dict["doc_id"].get_query = function(){
 			if (frm.fields_dict["doc_type"].value === "User") {
 				return {
