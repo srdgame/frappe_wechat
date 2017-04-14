6115a2032a04db7ee2237fbf2cec6087ed93a1ba
Dirk Chang <dirk@kooiot.com>
2017-04-14 11:35:59 +0800

    List all available pages

--- wechat/wechat/doctype/wechat_menu/wechat_menu.js
+++ wechat/wechat/doctype/wechat_menu/wechat_menu.js
@@ -3,6 +3,14 @@
 
 frappe.ui.form.on('Wechat Menu', {
 	refresh: function(frm) {
-
+		frappe.call({
+			type: 'GET',
+			method: "wechat.wechat.doctype.wechat_menu.wechat_menu.query_menu_routes",
+			callback: function (r, rt) {
+				if (r.message) {
+					frm.set_df_property('route', 'options', r.message);
+				}
+			}
+		});
 	}
 });
--- wechat/wechat/doctype/wechat_menu/wechat_menu.py
+++ wechat/wechat/doctype/wechat_menu/wechat_menu.py
@@ -3,8 +3,27 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+import os
 import frappe
 from frappe.model.document import Document
 
 class WechatMenu(Document):
 	pass
+
+
+@frappe.whitelist()
+def query_menu_routes():
+	app_path = frappe.get_app_path('wechat')
+	folders = frappe.local.flags.web_pages_folders or ('www', 'templates/pages')
+	routes = ['']
+	prefix = 'wechat_'
+	for start in folders:
+		search_path = os.path.join(app_path, start)
+		for root, dirs, files in os.walk(search_path):
+			for file in files:
+				if file[-4:] != 'html':
+					continue
+				if len(file) > len(prefix) and file[0:len(prefix)] == prefix:
+					routes.append(file[len(prefix):-5])
+
+	return routes
