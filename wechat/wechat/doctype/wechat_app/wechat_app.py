e7c2b71342bee18959189d75c5877ffa1debc70d
Dirk Chang <dirk@kooiot.com>
2017-04-28 16:20:03 +0800

    Create wechat auth file

--- wechat/wechat/doctype/wechat_app/wechat_app.py
+++ wechat/wechat/doctype/wechat_app/wechat_app.py
@@ -8,7 +8,19 @@ from frappe.model.document import Document
 
 class WechatApp(Document):
 	def on_update(self):
+		self.create_auth_file()
 		self.update_menu()
 
 	def update_menu(self):
 		frappe.enqueue('wechat.api.create_wechat_menu', app_name=self.name)
+
+	def create_auth_file(self):
+		frappe.delete_doc("Wechat Auth File", self.name, ignore_permissions=True)
+		doc = frappe.get_doc({
+			"doctype": "Wechat Auth File",
+			"title": self.name,
+			"route": self.file_name,
+			"app": self.name,
+			"content": self.file_content,
+		})
+		doc.insert(ignore_permissions=True)
---
+++ wechat/wechat/doctype/wechat_auth_file/wechat_auth_file.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017, Dirk Chang and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.website.website_generator import WebsiteGenerator
+
+class WechatAuthFile(WebsiteGenerator):
+	pass
