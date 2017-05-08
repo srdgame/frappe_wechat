f3e6f709cfe73dd99275bb99a983a8e24f261d9d
Dirk Chang <dirk@kooiot.com>
2017-05-08 11:17:29 +0800

    Menu for redirect to website homepage

---
+++ wechat/templates/pages/wechat_website_home.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017, Dirk Chang and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from wechatpy.oauth import WeChatOAuth
+from wechat.api import check_wechat_binding
+
+
+def get_context(context):
+	context.no_cache = 1
+
+	check_wechat_binding(redirect_url="/")
