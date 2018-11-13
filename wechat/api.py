6aed18e6c40f346c612e4ec324f2be8427a9986f
Dirk Chang <dirk@kooiot.com>
2018-11-13 18:49:00 +0800

    improve the wechat api

--- wechat/api.py
+++ wechat/api.py
@@ -157,9 +157,9 @@ def check_bind(app, openid, gen_token=False):
 	user = frappe.get_value('Wechat Binding', {'app': app, 'openid': openid}, 'user')
 	if not user:
 		if frappe.get_value('Wechat App', app, 'name'):
-			throw(_('Wechat Application does not exits'))
+			throw(_("There is no user bind with this openid!"))
 		else:
-			throw(_("Openid is not bind with any user"))
+			throw(_('Wechat application name does not exists!'))
 
 	token = frappe.get_value("IOT User Api", user, 'authorization_code')
 
