3f13b2f4b3c9efc93b5d88fa67832b3e45c9272c
Dirk Chang <dirk@kooiot.com>
2019-01-02 17:48:25 +0800

    update setup.py for latest frappe framework

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 import re, ast
 
-# get version from __version__ variable in wechat/__init__.py
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
+
+# get version from __version__ variable in conf_center/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('wechat/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
-
-requirements = parse_requirements("requirements.txt", session="")
+	version = str(ast.literal_eval(_version_re.search(
+		f.read().decode('utf-8')).group(1)))
 
 setup(
 	name='wechat',
 	version=version,
-	description='Wechat Integration',
-	author='Dirk Chang',
-	author_email='dirk.chang@symid.com',
+	description='Wechat module for FreeIOE Cloud',
+	author='freeioe.org',
+	author_email='dirk@kooiot.com',
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
 )
