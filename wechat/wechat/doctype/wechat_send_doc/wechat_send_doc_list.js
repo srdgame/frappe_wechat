f5795727b007b2077c1b35d8dc99499626269352
Dirk Chang <dirk@kooiot.com>
2019-04-22 12:37:22 +0800

    update list js for wechat send doc

--- wechat/wechat/doctype/wechat_send_doc/wechat_send_doc_list.js
+++ wechat/wechat/doctype/wechat_send_doc/wechat_send_doc_list.js
@@ -2,14 +2,23 @@
  * Created by cch on 17-3-16.
  */
 frappe.listview_settings['Wechat Send Doc'] = {
-	get_indicator: function(doc) {
-		colour = {'New': 'blue', 'Partial': 'orange', 'Finished': 'green', 'Error': 'red'};
-		return [__(doc.status), colour[doc.status], "status,=," + doc.status];
-	},
 	onload: function(me) {
-		frappe.route_options = {
-			"status": ['in', "New,Partial,Error"]
-		};
+		if (!frappe.route_options) {
+			frappe.route_options = {
+				"status": ['in', "New,Partial,Error"]
+			};
+		}
+	},
+	get_indicator: function(doc) {
+		if(doc.status == "New") {
+			return [__("New"), "bue"];
+		}else if(doc.status == "Partial") {
+			return [__("Partial"), "orange"];
+		} else if(doc.status == "Finished") {
+			return [__("Finished"), "green"];
+		} else if(doc.status == "Error") {
+			return [__("Error"), "red"];
+		}
 	},
 	add_fields: ["document_type", "document_id"],
-}
+};
